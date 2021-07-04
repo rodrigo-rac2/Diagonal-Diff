15756d6acf779c21712d22b7568c5874348c59fc
Rodrigo Costa <60664391+rodrigo-rac2@users.noreply.github.com>
2021-07-04 14:37:55 -0300

    Add project files.

---
+++ Solution.cs
@@ -0,0 +1,57 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace Diagonal_Diff
+{
+    class Result
+    {
+
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            int first = 0, second = 0, length = arr.Count;
+            for (int i = 0; i < length; i++)
+            {
+                first += arr[i][i];
+                second += arr[i][length - i - 1];
+            }
+            return Math.Abs(first - second);
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<List<int>> arr = new List<List<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+            }
+
+            int result = Result.diagonalDifference(arr);
+
+            Console.WriteLine(result);
+
+            // textWriter.Flush();
+            // textWriter.Close();
+        }
+    }
+
+}
